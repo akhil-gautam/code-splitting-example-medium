a43918ab4f489b0e12232e82c0600788a5f2186c
akhil-gautam <akhilg@mindfiresolutions.com>
2019-05-12 13:09:37 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+// importing other components using Dynamic Import
+const About = lazy(() =>
+  import(/* webpackChunkName: "About" */ './Components/About'));
+
+const Contact = lazy(() =>
+  import(/* webpackChunkName: "Contact" */ './Components/ContactUs'));
+
+const Home = () => {
   return (
     <div className="App">
+      <div style={{border: '2px solid blue', background: 'yellow', display: 'grid', gridGap: '1rem'}}>
+        <Link to='/about'>About</Link>
+        <Link to='/contact'>Contact</Link>
+      </div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -22,5 +35,17 @@ function App() {
     </div>
   );
 }
+
+const App = () => (
+  <Router>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route path="/about" component={About}/>
+        <Route path="/contact" component={Contact}/>
+      </Switch>
+    </Suspense>
+  </Router>
+);
 
 export default App;
---
+++ src/Components/ContactUs.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const ContactUs = () => (
+  <div style={{border: '2px solid black'}}>
+    <h1>
+      Contact Us
+    </h1>
+    <div>
+      Lorem Ipsum Press is licensed by 
+      <a href="http://www.bionetwork.gr" target="_blank">Bionetwork Ltd.</a> 
+      Our office is located within the company's building.<br/><br/>
+    </div>
+  </div>
+);
+
+export default ContactUs;
